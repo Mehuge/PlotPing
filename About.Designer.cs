b0ac6bea35c992feb312c5beb3b76f7e47acb38d
Mehuge <adf@sorcerer.co.uk>
2025-02-08 18:22:55 +0000

    Add donation button in about dialog

--- About.Designer.cs
+++ About.Designer.cs
@@ -32,13 +32,15 @@
             this.product = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.version = new System.Windows.Forms.Label();
+            this.donate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // product
             // 
             this.product.AutoSize = true;
             this.product.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.product.Location = new System.Drawing.Point(343, 104);
+            this.product.Location = new System.Drawing.Point(340, 39);
             this.product.Name = "product";
             this.product.Size = new System.Drawing.Size(208, 54);
             this.product.TabIndex = 3;
@@ -48,7 +50,7 @@
             // 
             this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
             this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panel1.Location = new System.Drawing.Point(58, 95);
+            this.panel1.Location = new System.Drawing.Point(59, 39);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(257, 261);
             this.panel1.TabIndex = 2;
@@ -57,17 +59,48 @@
             // 
             this.version.AutoSize = true;
             this.version.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.version.Location = new System.Drawing.Point(346, 158);
+            this.version.Location = new System.Drawing.Point(343, 107);
             this.version.Name = "version";
             this.version.Size = new System.Drawing.Size(54, 32);
             this.version.TabIndex = 4;
             this.version.Text = "1.0";
+            // 
+            // donate
+            // 
+            this.donate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.donate.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("donate.BackgroundImage")));
+            this.donate.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.donate.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.donate.FlatAppearance.BorderSize = 0;
+            this.donate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.donate.Location = new System.Drawing.Point(120, 350);
+            this.donate.Name = "donate";
+            this.donate.Size = new System.Drawing.Size(566, 56);
+            this.donate.TabIndex = 5;
+            this.donate.UseVisualStyleBackColor = false;
+            this.donate.Click += new System.EventHandler(this.donate_Click);
+            this.donate.MouseEnter += new System.EventHandler(this.donate_MouseEnter);
+            this.donate.MouseLeave += new System.EventHandler(this.donate_MouseLeave);
+            this.donate.MouseHover += new System.EventHandler(this.donate_MouseHover);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(349, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(388, 139);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Plot Ping was created in my spare time. If you find it useful and would like to m" +
+    "ake a small donation as a way of thanks, then please use the PayPal donation but" +
+    "ton below.";
             // 
             // About
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.donate);
             this.Controls.Add(this.version);
             this.Controls.Add(this.product);
             this.Controls.Add(this.panel1);
@@ -90,5 +123,7 @@
         private System.Windows.Forms.Label product;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label version;
+        private System.Windows.Forms.Button donate;
+        private System.Windows.Forms.Label label1;
     }
 }
--- About.cs
+++ About.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Reflection;
@@ -18,10 +19,32 @@ namespace PlotPingApp
             return Assembly.GetExecutingAssembly().GetName().Version.ToString();
         }
 
+        private static string donationLink = "https://www.paypal.com/donate/?business=GU7TQQQ3FABSL&no_recurring=0&item_name=Every+coffee+%E2%98%95+you+send+fuels+the+next+version.%0AThank+you+for+being+part+of+our+journey%21&currency_code=GBP";
+
         public About()
         {
+
             InitializeComponent();
             version.Text = $"Version {GetVersion()}";
+        }
+
+        private void donate_Click(object sender, EventArgs e)
+        {
+            // Open URL in the default browser
+            Process.Start(new ProcessStartInfo(donationLink) { UseShellExecute = true });
+        }
+
+        private void donate_MouseHover(object sender, EventArgs e)
+        {
+        }
+
+        private void donate_MouseEnter(object sender, EventArgs e)
+        {
+        }
+
+        private void donate_MouseLeave(object sender, EventArgs e)
+        {
+
         }
     }
 }
