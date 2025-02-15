29b378a0e50f92988889191111d6ecf258a8f40d
Mehuge <adf@sorcerer.co.uk>
2025-02-15 17:57:21 +0000

    Version 1.0.147

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.144.25046")]
-[assembly: AssemblyFileVersion("1.0.144.25046")]
-[assembly: AssemblyInformationalVersion("1.0.144.25046")]
+[assembly: AssemblyVersion("1.0.147.25046")]
+[assembly: AssemblyFileVersion("1.0.147.25046")]
+[assembly: AssemblyInformationalVersion("1.0.147.25046")]
